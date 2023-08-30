76241e0f428fb28996e9306078a1ee38b28839ec
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-08-30 21:36:01 +0300

    feat: js quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,38 @@
-import { useState } from 'react'
-
-import { useDispatch, useSelector } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Layout } from './components/layout/layout'
+import { JsPage } from './pages/js-page/js-page'
 import { QuizDirectionSelectorPage } from './pages/quiz-direction-selector-page/quiz-direction-selector-page'
-import { CheckResultAC, QuizStateTypeReducer, RemoveQuestionAC } from './state/questionReducer'
-import { RootStateType } from './state/store'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <QuizDirectionSelectorPage />,
+      },
+      {
+        path: '/js',
+        element: <JsPage />,
+      },
+      {
+        path: '/react',
+        element: <div>React</div>,
+      },
+      {
+        path: '/redux',
+        element: <div>Redux</div>,
+      },
+    ],
+  },
+])
 
 export const App = () => {
-  const state = useSelector<RootStateType, QuizStateTypeReducer>(state => state.questionReducer)
-  const dispatch = useDispatch()
-  // Количество вопросов в одном тесте будет 10
-  const [count, setCount] = useState<number>(1) // будем считать ответы
-
-  const removeQuestionHandler = (id: string) => {
-    dispatch(RemoveQuestionAC(id))
-  }
-  const checkResult = (id: string, questionID: number, num: number) => {
-    dispatch(CheckResultAC(id, questionID, num))
-  }
-  const routes = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <QuizDirectionSelectorPage />,
-        },
-        {
-          path: '/js',
-          element: <div>js</div>,
-        },
-        {
-          path: '/react',
-          element: <div>React</div>,
-        },
-        {
-          path: '/redux',
-          element: <div>Redux</div>,
-        },
-      ],
-    },
-  ])
-
   return (
     <>
       <RouterProvider router={routes} />
-      {/*<Header />*/}
-      {/*<Progress count={count} />*/}
-      {/*<div className={s.app}>*/}
-      {/*  {*/}
-      {/*    count <= 10 ? (*/}
-      {/*      <>*/}
-      {/*        <Questions*/}
-      {/*          state={state.quizState}*/}
-      {/*          setCount={setCount}*/}
-      {/*          count={count}*/}
-      {/*          removeQuestionHandler={removeQuestionHandler}*/}
-      {/*          checkResult={checkResult}*/}
-      {/*        />*/}
-
-      {/*        <span>Question: {count}</span>*/}
-      {/*      </>*/}
-      {/*    ) : (*/}
-      {/*      <EndQuiz />*/}
-      {/*    )*/}
-      {/*    // <span>Correct: {state.count}</span>*/}
-      {/*  }*/}
-      {/*</div>*/}
     </>
   )
 }
---
+++ src/pages/js-page/js-page.tsx
@@ -0,0 +1,34 @@
+import { FC, useState } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+
+import { Page } from '../../components'
+import { Questions } from '../../components/Game/Questions/Questions'
+import { CheckResultAC, QuizStateTypeReducer, RemoveQuestionAC } from '../../state/questionReducer'
+import { RootStateType } from '../../state/store'
+
+export const JsPage: FC = () => {
+  const state = useSelector<RootStateType, QuizStateTypeReducer>(state => state.questionReducer)
+  const dispatch = useDispatch()
+  // Количество вопросов в одном тесте будет 10
+  const [count, setCount] = useState<number>(1) // будем считать ответы
+
+  const removeQuestionHandler = (id: string) => {
+    dispatch(RemoveQuestionAC(id))
+  }
+  const checkResult = (id: string, questionID: number, num: number) => {
+    dispatch(CheckResultAC(id, questionID, num))
+  }
+
+  return (
+    <Page>
+      <Questions
+        state={state.quizState}
+        setCount={setCount}
+        count={count}
+        removeQuestionHandler={removeQuestionHandler}
+        checkResult={checkResult}
+      />
+    </Page>
+  )
+}
